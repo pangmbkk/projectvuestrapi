b1257ab3fb02f33ab2334fb2f65aa454da75b6c0
Erfan Khareng <erfan.mbkk@gmail.com>
2019-07-15 12:35:58 +0700

    project vuestrapi v.2 //update axios for get data from server

--- client/pages/about.vue
+++ client/pages/about.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
-    <navbar />
+
     <h1>This is an about page</h1>
-    <Footer />
+   
   </div>
 </template>
--- client/pages/search.vue
+++ client/pages/search.vue
@@ -11,18 +11,15 @@
                     <div class="col-4">
                         <select class="form-control form-control-sm">
                             <option selected >--เลือกจังหวัดของคุณ--</option>
-                            <option>สงขลา</option>
-                            <option>กรุงเทพฯ</option>
-                            <option>เชียงใหม่</option>
+                            <option v-for="province in provinces" :key="province.id">{{province.name}}</option>
+                           
                         </select>
 
                     </div>
                     <div class="col-4">
                         <select class="form-control form-control-sm">
                             <option selected >--เลือกวิชา--</option>
-                            <option>ภาษาอังกฤษ</option>
-                            <option>ฟิสิกส์</option>
-                            <option>math</option>
+                            <option v-for="subjectname in subjectnames" :key="subjectname.id">{{subjectname.name}}</option>
                         </select>
                     </div>
                     <button class="btn btn-primary btn-sm col" type="submit">ค้นหา</button>
@@ -32,12 +29,10 @@
                 <div class="form-group row">
                     <div class="col-1"></div>
 
-                    <div class="col-4">
+                    <div class="col-4 ">
                         <select class="form-control form-control-sm">
                             <option selected >--ระดับชั้น--</option>
-                            <option>ประถมปีที่ 1</option>
-                            <option>มัธยมปีที่ 1</option>
-                            <option>มหาวิทยาลัยปีที่ 1 </option>
+                            <option v-for="educationlevel in educationlevels " :key="educationlevel.id">{{educationlevel.name}}</option>
                         </select>
 
                     </div>
@@ -66,30 +61,30 @@
                 <div class="row">
                     <div class="col-9">
                         <div >
-                            <div     >   
+                            <div v-for="info in infos" :key="info.id"    >   
                                 <hr>
                                 <router-link to="/" tag="a" class="router" >  
                                 <div class="row pointer"  >
                                     
                                                   
                                     <div class="col-3 "  >
-                                            <img class="card-img-left img-fluid" src="../assets/img/2.svg"  alt="">
+                                            <img class="card-img-left img-fluid" :src="'http://localhost:1337' + info.image.url"  alt="">
                                                                              
                                         
                                     </div>
 
                                     <div class="col">
-                                        <h4 class="title"> 555   </h4>
+                                        <h4 class="title"> {{info.name}}   </h4>
                                         <br>
                                         <p>
-                                            66666
+                                            {{info.description}}
                                         </p>
                                     </div>
 
                                     <div class="col-2">
                                         <h4>ประกาศของ</h4>
                                         <p class="card-text"   >
-                                             'No description provided' }}
+                                           {{ info.user.username ||'No description provided' }}
                                         </p>
                                     </div>
                                      
@@ -180,18 +175,48 @@ const axios = require('axios');
 export default {
     data() {
     return {
-        info:[]
-       
+        infos:[],
+        info:{
+                id:'0',
+                name:'',
+                description:''
+            
+            },
+       provinces:[],
+       subjectnames:[],
+       educationlevels:[]
     }
   },
 
     methods:{
     
-      async getData(){
-          axios
-                .get('http://localhost:1337/provinces')
-                .then(response => (this.info = response.data))
-      }
+        async getData(){
+            axios
+                .get('http://localhost:1337/announcementposts')
+                .then(response => {
+                    this.infos = response.data ;
+                    console.log('get Announcementposts success')
+                });
+            axios.get('http://localhost:1337/provinces').then(res=>{
+                this.provinces = res.data
+                console.log('get provinces success')
+            })
+            axios.get('http://localhost:1337/subjectnames').then(res=>{
+                this.subjectnames = res.data
+                console.log('get subjectnames success')
+            })
+            axios.get('http://localhost:1337/educationlevels ').then(res=>{
+                this.educationlevels = res.data
+                console.log('get educationlevels success')
+            })
+            
+      },
+    },
+    mounted(){
+        this.getData();
+        
+        
+
     }
     
 
